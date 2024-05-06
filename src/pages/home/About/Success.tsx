a15ac967f68be733b09755266ad0b429adca9c85
Suyel Haque <slsuyel@gmail.com>
2024-05-06 18:25:58 +0600

    animations

--- src/pages/home/About/Success.tsx
+++ src/pages/home/About/Success.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 interface SuccessData {
   title: string;
+  date: string;
   content: string;
   imageSrc: string;
 }
@@ -16,20 +17,29 @@ const Success = () => {
   }, []);
 
   return (
-    <div className="container my-5">
-      <div className="row">
+    <div className=" my-5 p-3 py-5">
+      <div className="row mx-auto ">
         {data.map((item, index) => (
           <>
             <div className="col-md-6" key={index}>
               <div className="content">
                 <h1>{item.title}</h1>
-                <p>{item.content}</p>
+                <div className="d-flex fs-5 justify-content-between mb-3 px-1">
+                  <p className="my-1">
+                    <i className="fa-regular fa-calendar-days"></i>{" "}
+                    Dhaka,Bangladesh
+                  </p>
+                  <p className="my-1">
+                    <i className="fa-regular fa-calendar-days"></i> {item.date}
+                  </p>
+                </div>
+                <p className="fs-5">{item.content}</p>
               </div>
             </div>
             <div className="col-md-6" key={index}>
               <div className="sticky-image-container">
                 <img
-                  className="img-fluid mt-5 sticky-image"
+                  className="img-fluid mt-5 sticky-image "
                   src={item.imageSrc}
                   alt=""
                 />
