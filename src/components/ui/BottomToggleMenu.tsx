17482fc518dda355a258a0f2463579fa02145be6
Suyel Haque <slsuyel@gmail.com>
2024-05-06 13:15:45 +0600

    Draggable menu added

--- src/components/ui/BottomToggleMenu.tsx
+++ src/components/ui/BottomToggleMenu.tsx
@@ -1,8 +1,8 @@
+import Draggable from "react-draggable";
 import { useState } from "react";
 import { Menu } from "antd";
 import icon from "../../assets/icons/menubtn.png";
 import "../Styles/Components.css";
-
 const BottomToggleMenu = () => {
   const [visible, setVisible] = useState(false);
 
@@ -19,47 +19,50 @@ const BottomToggleMenu = () => {
         textAlign: "center",
       }}
     >
-      <img
-        className="menu-box-img"
-        src={icon}
-        alt=""
-        onClick={handleMenuClick}
-        style={{
-          width: 70,
-          position: "fixed",
-          bottom: 5,
-          left: "50%",
-          transform: "translateX(-50%)",
-          zIndex: 1,
-        }}
-      />
-
-      <Menu
-        className="menu-toggle font-maven fs-5"
-        mode="vertical"
-        onClick={handleMenuClick}
-        style={{
-          position: "fixed",
-          bottom: visible ? "50px" : "-500px",
-          zIndex: 0,
-          transform: "translateX(-50%)",
-        }}
-      >
-        <form className="align-items-center bg-body d-flex gap-2 px-2 rounded">
-          <i className="fas fa-search text-info-emphasis" />
-          <input
-            className="border-0 py-1 text-center w-100 tgl-menu-srch text-info-emphasis"
-            type="search"
-            placeholder="Search "
+      <Draggable>
+        <div>
+          <img
+            className="menu-box-img"
+            src={icon}
+            alt=""
+            onClick={handleMenuClick}
+            style={{
+              width: 70,
+              position: "fixed",
+              bottom: 5,
+              left: "50%",
+              transform: "translateX(-50%)",
+              zIndex: 1,
+            }}
           />
-        </form>
+          <Menu
+            className="menu-toggle font-maven fs-5"
+            mode="vertical"
+            onClick={handleMenuClick}
+            style={{
+              position: "fixed",
+              bottom: visible ? "50px" : "-500px",
+              zIndex: 0,
+              transform: "translateX(-50%)",
+            }}
+          >
+            <form className="align-items-center bg-body d-flex gap-2 px-2 rounded">
+              <i className="fas fa-search text-info-emphasis" />
+              <input
+                className="border-0 py-1 text-center w-100 tgl-menu-srch text-info-emphasis"
+                type="search"
+                placeholder="Search "
+              />
+            </form>
 
-        <Menu.Item key="models">Our Models</Menu.Item>
-        <Menu.Item key="specialties">Specialties</Menu.Item>
-        <Menu.Item key="about">About</Menu.Item>
-        <Menu.Item key="blog">Blog</Menu.Item>
-        <Menu.Item key="contact">Contact</Menu.Item>
-      </Menu>
+            <Menu.Item key="models">Our Models</Menu.Item>
+            <Menu.Item key="specialties">Specialties</Menu.Item>
+            <Menu.Item key="about">About</Menu.Item>
+            <Menu.Item key="blog">Blog</Menu.Item>
+            <Menu.Item key="contact">Contact</Menu.Item>
+          </Menu>
+        </div>
+      </Draggable>
     </div>
   );
 };
