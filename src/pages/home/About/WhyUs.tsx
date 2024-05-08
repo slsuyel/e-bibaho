a801e0faf12933d074b721c3100ffce97ac03106
Suyel Haque <slsuyel@gmail.com>
2024-05-08 19:23:56 +0600

    Why Us ?

--- src/pages/home/About/WhyUs.tsx
+++ src/pages/home/About/WhyUs.tsx
@@ -40,168 +40,48 @@ const WhyUs = () => {
     };
   }, [scrollMode]);
 
+  const data = [1, 1, 1, 1, 11];
+
   return (
     <div className="bg-grad">
       <h1 className="position-relative py-4 text-center text-white text-wrap">
-        This is Why Us page
+        Why Us ?
       </h1>
 
       <main className="scroll-container my-2 ">
-        <section className="align-items-baseline hr-content justify-content-between row mx-auto px-4">
-          <div className=" col-md-6">
-            <div className="imgContainer">
-              <img
-                className="myImg"
-                src="https://bibahabd.net/wp-content/uploads/2021/11/Marriage-1.jpg"
-                alt="Heart-shaped image"
-              />
+        {data.map(() => (
+          <section className="align-items-baseline hr-content justify-content-between row mx-auto px-4">
+            <div className="col-md-6 d-flex justify-content-center">
+              <div className="imgContainer">
+                <img
+                  className="myImg"
+                  src="https://bibahabd.net/wp-content/uploads/2021/11/Marriage-1.jpg"
+                  alt="Heart-shaped image"
+                />
+              </div>
             </div>
-          </div>
 
-          <div className="content-wrapper col-md-6 text-end position-relative ">
-            <img
-              width={400}
-              height={400}
-              src="https://i.pinimg.com/originals/98/fc/40/98fc4058a79909e9298fb94c227adade.png"
-              alt=""
-              className="img-fluid "
-            />
-            <div className="content-txt">
-              <h2>Your Title</h2>
-              <p>
-                Your paragraph Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Aspernatur numquam nihil minima at possimus,
-                dolorum, vel ratione harum dolores aut, corporis optio
-                blanditiis nostrum deserunt ipsum libero omnis delectus
-                mollitia?
-              </p>
-            </div>
-          </div>
-        </section>
-        <section className="align-items-baseline hr-content justify-content-between row mx-auto px-4">
-          <div className=" col-md-6">
-            <div className="imgContainer">
+            <div className="content-wrapper col-md-6 text-end position-relative ">
               <img
-                className="myImg"
-                src="https://bibahabd.net/wp-content/uploads/2021/11/Marriage-1.jpg"
-                alt="Heart-shaped image"
+                width={400}
+                height={400}
+                src="https://i.pinimg.com/originals/98/fc/40/98fc4058a79909e9298fb94c227adade.png"
+                alt=""
+                className="img-fluid "
               />
+              <div className="content-txt">
+                <h2>Your Title</h2>
+                <p>
+                  Your paragraph Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Aspernatur numquam nihil minima at possimus,
+                  dolorum, vel ratione harum dolores aut, corporis optio
+                  blanditiis nostrum deserunt ipsum libero omnis delectus
+                  mollitia?
+                </p>
+              </div>
             </div>
-          </div>
-
-          <div className="content-wrapper col-md-6 text-end position-relative ">
-            <img
-              width={400}
-              height={400}
-              src="https://i.pinimg.com/originals/98/fc/40/98fc4058a79909e9298fb94c227adade.png"
-              alt=""
-              className="img-fluid "
-            />
-            <div className="content-txt">
-              <h2>Your Title</h2>
-              <p>
-                Your paragraph Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Aspernatur numquam nihil minima at possimus,
-                dolorum, vel ratione harum dolores aut, corporis optio
-                blanditiis nostrum deserunt ipsum libero omnis delectus
-                mollitia?
-              </p>
-            </div>
-          </div>
-        </section>
-        <section className="align-items-baseline hr-content justify-content-between row mx-auto px-4">
-          <div className=" col-md-6">
-            <div className="imgContainer">
-              <img
-                className="myImg"
-                src="https://bibahabd.net/wp-content/uploads/2021/11/Marriage-1.jpg"
-                alt="Heart-shaped image"
-              />
-            </div>
-          </div>
-
-          <div className="content-wrapper col-md-6 text-end position-relative ">
-            <img
-              width={400}
-              height={400}
-              src="https://i.pinimg.com/originals/98/fc/40/98fc4058a79909e9298fb94c227adade.png"
-              alt=""
-              className="img-fluid "
-            />
-            <div className="content-txt">
-              <h2>Your Title</h2>
-              <p>
-                Your paragraph Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Aspernatur numquam nihil minima at possimus,
-                dolorum, vel ratione harum dolores aut, corporis optio
-                blanditiis nostrum deserunt ipsum libero omnis delectus
-                mollitia?
-              </p>
-            </div>
-          </div>
-        </section>
-        <section className="align-items-baseline hr-content justify-content-between row mx-auto px-4">
-          <div className=" col-md-6">
-            <div className="imgContainer">
-              <img
-                className="myImg"
-                src="https://bibahabd.net/wp-content/uploads/2021/11/Marriage-1.jpg"
-                alt="Heart-shaped image"
-              />
-            </div>
-          </div>
-
-          <div className="content-wrapper col-md-6 text-end position-relative ">
-            <img
-              width={400}
-              height={400}
-              src="https://i.pinimg.com/originals/98/fc/40/98fc4058a79909e9298fb94c227adade.png"
-              alt=""
-              className="img-fluid "
-            />
-            <div className="content-txt">
-              <h2>Your Title</h2>
-              <p>
-                Your paragraph Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Aspernatur numquam nihil minima at possimus,
-                dolorum, vel ratione harum dolores aut, corporis optio
-                blanditiis nostrum deserunt ipsum libero omnis delectus
-                mollitia?
-              </p>
-            </div>
-          </div>
-        </section>
-        <section className="align-items-baseline hr-content justify-content-between row mx-auto px-4">
-          <div className=" col-md-6">
-            <div className="imgContainer">
-              <img
-                className="myImg"
-                src="https://bibahabd.net/wp-content/uploads/2021/11/Marriage-1.jpg"
-                alt="Heart-shaped image"
-              />
-            </div>
-          </div>
-
-          <div className="content-wrapper col-md-6 text-end position-relative ">
-            <img
-              width={400}
-              height={400}
-              src="https://i.pinimg.com/originals/98/fc/40/98fc4058a79909e9298fb94c227adade.png"
-              alt=""
-              className="img-fluid "
-            />
-            <div className="content-txt">
-              <h2>Your Title</h2>
-              <p>
-                Your paragraph Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Aspernatur numquam nihil minima at possimus,
-                dolorum, vel ratione harum dolores aut, corporis optio
-                blanditiis nostrum deserunt ipsum libero omnis delectus
-                mollitia?
-              </p>
-            </div>
-          </div>
-        </section>
+          </section>
+        ))}
       </main>
     </div>
   );
